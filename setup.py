607fcc845073b23aa0709ee7701ec909f8a462cc
Dan Knutsen <dknutsen@gmail.com>
2016-02-01 10:41:39 -0600

    Changes some things around so it plays nicely with pip

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-#from distutils.core import setup
-from setuptools import setup
+from distutils.core import setup
+#from setuptools import setup
 
 setup(
-    name='CityTraderPythonLib',
-    version='0.1',
+    name='citytraderpythonlibpip',
+    version='0.3.1',
     install_requires=[
         "requests",
         "requests[security]",
     ],
-    packages=['citytrader',],
+    url='https://github.com/optionscity/city-trader-python-lib',
+    packages=['citytraderpythonlibpip',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.md').read(),
 )
